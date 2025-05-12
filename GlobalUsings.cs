4a3a92a6c8ff5248f3f845ee0ffa4e9ab83e385a
Matthew Gertz <mattge@microsoft.com>
2025-05-12 11:25:18 -0700

    Initial commit after converting to .NET 9 and SDK style projects

---
+++ GlobalUsings.cs
@@ -0,0 +1,15 @@
+// Global using directives for CSEuchre4
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Text;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Data;
+global using System.Windows.Documents;
+global using System.Windows.Input;
+global using System.Windows.Media;
+global using System.Windows.Media.Imaging;
+global using System.Windows.Navigation;
+global using System.Windows.Shapes;
